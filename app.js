15b78bb0a4960982609a9fac8f81fa7278450509
MefistoContreras <carlosteiker@gmail.com>
2020-04-30 00:59:31 -0500

    Resourced assets to dropbox and margin fixed

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("nav-bar").style.top = "0";
-  } else {
-    document.getElementById("nav-bar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}
+//Variables
+let prevScrollpos = window.pageYOffset;
+
+// EventListeners
+
+//Functions
+
+//Ocultar
+  window.setTimeout(function () {
+    window.onscroll = function () {
+      let currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById("nav-bar").style.top = "0";
+      } else {
+        document.getElementById("nav-bar").style.top = "-15%";
+      }
+      prevScrollpos = currentScrollPos;
+    }
+  }, 500);
